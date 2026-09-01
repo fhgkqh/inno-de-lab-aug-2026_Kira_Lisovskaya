881159c783f493673c6ad1d3bc4c2dba27b4e9cb
fhgkqh <r39182880@gmail.com>
2026-09-01 20:34:56 +0300

    теперь 1 int

--- homework_7_python_data_types/task2.py
+++ homework_7_python_data_types/task2.py
@@ -3,10 +3,10 @@ raw_transactions = ["SUCCESS:100", "FAILED:50", "SUCCESS:-10", "SUCCESS:0", "SUC
 
 # Генератор списка для фильтрации транзакций:
 filtered = [
-    int(t.split(':')[1])          # разбиваем строку по ':', берём вторую часть, преобразуем в int
-    for t in raw_transactions     # перебираем все транзакции
-    if t.startswith("SUCCESS:")   # оставляем только те, которые начинаются на SUCCESS:
-    and int(t.split(':')[1]) > 0  # проверка что сумма больше нуля
+    amount                             # переменная для суммы транзакциии
+    for t in raw_transactions          # перебираем все транзакции
+    if t.startswith("SUCCESS:")        # оставляем только те, которые начинаются на SUCCESS:
+    and (amount := int(parts[1])) > 0  # проверка что сумма больше нуля
 ]
 
 # Вывод результата
